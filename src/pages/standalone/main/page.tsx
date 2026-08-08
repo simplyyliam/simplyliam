8d31407cd9bc2aab4254dcce895086dbb17283cf
Liam <138723425+simplyyliam@users.noreply.github.com>
2026-08-08 20:28:05 +0200

    style: slimmed down the horizontal width, and increase top padding

--- src/pages/standalone/main/page.tsx
+++ src/pages/standalone/main/page.tsx
@@ -4,9 +4,9 @@ import { Banner } from "@/widgets/banner";
 export default function Main() {
   return (
     <main className="flex min-h-dvh w-full justify-center sm:px-6 sm:py-2.5 md:px-10 lg:px-16">
-      <div className="flex min-h-0 w-full max-w-264 flex-1 flex-col items-start gap-8 pb-8 sm:gap-10 sm:pb-10 lg:gap-12.5">
-        <Banner />
+      <div className="flex min-h-0 w-full max-w-174 flex-1 flex-col items-start gap-8 pb-8 pt-40 sm:gap-10 sm:pb-10 lg:gap-12.5">
         <About />
+        <Banner />
         <ProjectsSection />
       </div>
     </main>
